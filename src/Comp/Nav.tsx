bed51a489be54886147f051a0afe96e9a27b01b6
dharmender0801 <teamsoul234@gmail.com>
2023-10-28 23:15:47 +0530

    new addition

--- src/Comp/Nav.tsx
+++ src/Comp/Nav.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 const NAV = () => {
     return (
-        <nav className="flex-div ">
-            <div className="left-container">
-                <h1 className="glowing-text">NOVO RESUME</h1>
+        <>
+            <nav className="nav-container">
+                <div className="inside-nav">
+                    {/* <div className="left-container">
+                        <h1>This is Testing</h1>
+                    </div> */}
+                </div>
+            </nav>
+            <div className="bg">
+                <header id="hex-grid">
+                    <div className="light"></div>
+                    <div className="grid"></div>
+                </header>
+
             </div>
-        </nav>
+        </>
+
     )
 }
 export default NAV;
