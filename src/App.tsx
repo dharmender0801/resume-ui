194c935e2221a40bd0d93854bd37629add369c31
dharmender0801 <teamsoul234@gmail.com>
2023-10-28 14:31:17 +0530

    new nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
+import NAV from './Comp/Nav';
 
 function App() {
   return (
-    <div className="App">
-     <h1>cheking is working or  snot</h1>
-    </div>
+    
+     <NAV />
+    
   );
 }
 
---
+++ src/Comp/Nav.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+const NAV = () => {
+    return (
+        <nav className="flex-div ">
+            <div className="left-container">
+                <h1 className="glowing-text">NOVO RESUME</h1>
+            </div>
+        </nav>
+    )
+}
+export default NAV;
